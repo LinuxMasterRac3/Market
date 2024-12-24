39eb86d138cf4145ded37ea9002184466a8800be
LinuxMasterRac3 <bufalino.david@gmail.com>
2024-12-24 14:39:07 +0100

    Initial commit for GestionaleJavawebUI project[B

--- comandi/compile.sh
+++ comandi/compile.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+cd /app
 # Remove all .class files
-find ./src/com/example/ -name "*.class" -delete
+find src/com/example/ -name "*.class" -delete
 
 # Compile all Java files with json jar in classpath
-javac -cp ./lib/json-20240303.jar:./src $(find ./src/com/example -name "*.java")
+javac -cp lib/json-20240303.jar:src $(find src/com/example -name "*.java")
